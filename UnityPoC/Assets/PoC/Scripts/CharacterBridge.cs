4d2e3d104c5381245564459ea87357539404dd2f
hwankeee <kee@voyagergames.gg>
2026-03-09 13:41:27 +0900

    Update Character Bridge

--- UnityPoC/Assets/PoC/Scripts/CharacterBridge.cs
+++ UnityPoC/Assets/PoC/Scripts/CharacterBridge.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
 
-public class CharacterBridge
+namespace PoC
 {
-    
+    public class CharacterBridge : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+
+        // Android에서 UnitySendMessage로 호출
+        void PlayEmotion(string emotionName)
+        {
+            Debug.Log($"[PoC] PlayEmotion: {emotionName}");
+            animator.SetTrigger(emotionName);
+        }
+
+        void OnUnityLoaded(string _)
+        {
+            Debug.Log("[PoC] Unity loaded and ready");
+        }
+    }
 }
